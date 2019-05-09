5dabf158a3b9d59f2d2deb0417db24b2b75b2f00
bustling-maker-4835 <chopra.chet@gmail.com>
2019-05-09 20:24:14 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -27,5 +27,5 @@ def bonus
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy[:montague][:hero][:status] = "dead"
   epic_tragedy[:capulet][:heroine][:status] = "dead"
-  return epic_tragedy[:montague][:patriarch][:name]
+  return epic_tragedy
 end
